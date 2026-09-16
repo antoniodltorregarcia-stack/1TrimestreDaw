23c72205df237ec67d6eed22a3864a8e7f3b15c1
Tony <antoniodltorregarcia@gmail.com>
2026-09-16 15:26:41 +0200

    Ejemplo Hola Mundo 10 veces

---
+++ Programacion/ProyectosBlueJ/Ejemplo/Ejemplo.java
@@ -0,0 +1,22 @@
+
+/**
+ * Clase de Ejemplo para conocer la estructura
+ * 
+ * @author (Tony) 
+ * @version (1.0)
+ */
+public class Ejemplo {
+    
+    public static void main (String[] args) {
+        
+        //Aquí hago cosas
+        
+        int i = 0;
+        
+        while (i<10) {
+            System.out.println("Hello world");
+            i++;
+        }
+        
+    }
+}
